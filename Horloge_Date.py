827a80ffb23894ba63f8848ddadf07738ff78097
lenny <lennyrappro@gmail.com>
2025-04-16 22:52:56 +0200

    ⏰/📅 Add fonction du moment de la journée

--- Horloge_Date.py
+++ Horloge_Date.py
@@ -1,8 +1,17 @@
 from datetime import datetime
-import time
 
-heure_actuel = datetime.now()
-date_actuel = time
 
-print("Heure actuel :", heure_actuel.strftime("%H:%M:%S"))
-print("Date actuel :", date_actuel.strftime("%d/%m/%Y"))
+def momentJournee(heure):
+    if 5 >= heure <= 12:
+        return "le matin"
+    elif 13 >= heure <= 18:
+        return "l'après midi"
+    else:
+        return "le soir"
+
+datetime = datetime.now()
+heure = datetime.hour
+
+print("Heure actuel :", datetime.strftime("%H:%M:%S"))
+print("Date actuel :", datetime.strftime("%d/%m/%Y"))
+print("C'est",momentJournee(heure))
