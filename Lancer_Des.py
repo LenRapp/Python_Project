0d8e28b0db128434b5c42c0e7bd64c8d17262ba6
lenny <lennyrappro@gmail.com>
2025-04-17 22:18:43 +0200

    🎲 Jeu de dés

---
+++ Lancer_Des.py
@@ -0,0 +1,16 @@
+import random
+
+while True:
+    nbLancer = int(input("Combien de lancer de dés : "))
+    nbFaces = int(input("Combien de faces : "))
+
+    resultat = []
+    for i in range(nbLancer):
+        resultat.append(random.randint(1, nbFaces))
+    print("Résultats des lancers : ", resultat)
+    print("Somme :", sum(resultat))
+
+    relancer = input("Tu veux relancer (y/n) :").lower()
+    if relancer != "y":
+        print("Fin du Jeu")
+        break
