fbdb719ee0a8a4b431b102f422606b52d5ad54d1
lenny <lennyrappro@gmail.com>
2025-04-20 19:11:11 +0200

    🌊 Piscine

---
+++ exercice/Piscine.py
@@ -0,0 +1,10 @@
+print("Bienvenue à la piscine !!!")
+print("Veulliez remplir quelleque informations")
+
+taille=(float(input("Votre taille : ")))
+
+
+if taille < 1.50:
+    print("Désolé vous ne pouvez pas accéder à ce bassin")
+else:
+    print("Vous pouvez accéder à ce bassin")
