77901c7bbe4231cf04ad674d2c5c43d044f30aeb
lenny <lennyrappro@gmail.com>
2025-04-28 16:48:19 +0200

    🆕 Version 2: SommeChiffre

--- exercice/SommeChiffre.py
+++ exercice/SommeChiffre.py
@@ -1,3 +1,4 @@
+# Version 2
 nb=int(input("Entrer un entier : "))
 nbChaine = str(nb)
 somme = 0
@@ -5,5 +6,15 @@ liste_chiffre = []
 for e in nbChaine:
     liste_chiffre.append(e)
     somme += int(e)
-calcule = " + ".join(liste_chiffre)
-print("La somme de ses chiffres est",calcule,"=",somme)
+print("La somme de ses chiffres est",somme)
+
+# Version 1
+# nb=int(input("Entrer un entier : "))
+# nbChaine = str(nb)
+# somme = 0
+# liste_chiffre = []
+# for e in nbChaine:
+#     liste_chiffre.append(e)
+#     somme += int(e)
+# calcule = " + ".join(liste_chiffre)
+# print("La somme de ses chiffres est",calcule,"=",somme)
