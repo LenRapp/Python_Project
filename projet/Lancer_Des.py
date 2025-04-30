db321ac6794bfd136f6fd7665c47b0d47b6cda53
lenny <lennyrappro@gmail.com>
2025-04-30 11:12:29 +0200

    ✨🎲 Jeu de dés

--- projet/Lancer_Des.py
+++ projet/Lancer_Des.py
@@ -7,8 +7,13 @@ while True:
     resultat = []
     for i in range(nbLancer):
         resultat.append(random.randint(1, nbFaces))
+    somme = 0
+    for e in resultat:
+        somme += e
     print("Résultats des lancers : ", resultat)
-    print("Somme :", sum(resultat))
+    print("Somme :", somme)
+    # ici on utiilse la methode sum directement
+    #print("Somme :", sum(resultat))
 
     relancer = str(input("Tu veux relancer (y/n) :")).lower()
     if relancer != 'y':
